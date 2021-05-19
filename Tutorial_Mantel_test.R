7273fd4703bb55c83c17e1cac40e1d8bd07320c4
Brian Kim <briankimstudio@gmail.com>
2021-05-19 18:31:49 +0800

    Add vegan package

--- Tutorial_Mantel_test.R
+++ Tutorial_Mantel_test.R
@@ -1,36 +1,43 @@
 #HOW CAN I PERFORM A MANTEL TEST IN R? | R FAQ
 
 library(ade4)
+library(vegan)
 
-# Load data
+# Load data from Web
 ozone <- read.table("https://stats.idre.ucla.edu/stat/r/faq/ozone.csv", sep=",", header=T)
+# or Load data from local file
+#ozone <- read.table("ozone.csv", sep=",", header=T)
 
 # Print data
 str(ozone)
 head(ozone, n=10)
 
 # Calculate distance matrix for locations
-station.dists <- dist(cbind(ozone$Lon, ozone$Lat)) 
+#station.dists <- dist(cbind(ozone$Lon, ozone$Lat)) 
+station.dists <- dist(ozone[,c('Lon','Lat')]) # All rows from Lon, Lat columns
 
 # Calculate distance matrix for measurements
 ozone.dists <- dist(ozone$Av8top)
 
-library(ggplot2)
-library(reshape2)
-melted_cormat <- melt(as.matrix(station.dists))
-head(melted_cormat)
-ggplot(data = melted_cormat, aes(x=Var1, y=Var2, fill=value)) + 
-  geom_tile()
-
-melted_cormat <- melt(as.matrix(ozone.dists))
-head(melted_cormat)
-ggplot(data = melted_cormat, aes(x=Var1, y=Var2, fill=value)) + 
-  geom_tile()
+# Control matrix
+# Create a matrix arbitrarily for demonstration purpose only
+c.dists <-  dist(ozone$Av8top * runif(length(ozone$Av8top)))
 
 # Print matrix
 as.matrix(station.dists)[1:5, 1:5]
 as.matrix(ozone.dists)[1:5, 1:5]
+as.matrix(c.dists)[1:5, 1:5]
 
-# Run Mantel test
+# Using ade4 package
+# Run simple Mantel test
 results <- mantel.rtest(station.dists, ozone.dists, nrepet = 9999)
 show(results)
+
+# Using vegan package
+# Run simple Mantel test
+results <- mantel(station.dists, ozone.dists, permutations = 9999)
+show(results)
+
+# Run partial Mantel test
+results_partial <- mantel.partial(station.dists, ozone.dists, c.dists, permutations = 9999)
+show(results_partial)
